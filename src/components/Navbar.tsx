f5f839422032930313f94060aea7008be72949a1
unknown <ennyola45@gmail.com>
2026-02-10 09:59:42 +0100

    second adjustment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -149,7 +149,7 @@ const Navbar = () => {
                 onClick={getRandomQuote}
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
-                className="px-4 py-2 cursor-pointer bg-linear-to-r from-cyan-500 to-teal-400 text-white font-semibold rounded-full shadow-lg shadow-cyan-500/25 hover:shadow-xl hover:shadow-cyan-500/35 transition-all duration-300"
+                className="px-3 py-2 cursor-pointer text-white text-sm  rounded-full border border-gray-100 transition-all duration-300"
               >
                 Get Quote
               </motion.button>
